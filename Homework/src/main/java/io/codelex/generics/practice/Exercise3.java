7a7f664564afe1e09be65471f1e3664bb9c43148
Tang-Kai-1 <atlas190028@gmail.com>
2022-10-13 00:12:57 +0300

    pushed wrong solution for generics exercise 3

--- Homework/src/main/java/io/codelex/generics/practice/Exercise3.java
+++ Homework/src/main/java/io/codelex/generics/practice/Exercise3.java
@@ -3,9 +3,9 @@ package io.codelex.generics.practice;
 import java.util.List;
 
 public class Exercise3 {
-    public void add(Object value, List<?> list) {
+    public <T> void add(T value, List<T> list) {
         list.add(value);
-    }
+    }//This solution would be more restrictive but wildcards are too unpredictable so using generics would be better.
     public <V> void addAllX(List<V> v, List<? super V> l) {
         for(V el : v) l.add(el);
     }//This method is less restrictive because it allows not just a single class but also it's predecessors.
